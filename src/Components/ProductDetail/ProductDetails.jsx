525c51035260964046ed7d0b82021eb84e5814f3
ahmed mesalamy <102010510+SamMesalamy@users.noreply.github.com>
2023-09-23 00:28:49 +0300

    added related Products

--- src/Components/ProductDetail/ProductDetails.jsx
+++ src/Components/ProductDetail/ProductDetails.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styles from './product.module.css';
+import styles2 from '../Hotdeals/Hotdeals.module.css';
+
 import { Link } from "react-router-dom";
 import IncDec from "./IncDec";
 import { blue } from "@mui/material/colors";
@@ -25,17 +27,36 @@ export default function ProductDetails() {
     });
 });
 
-useEffect(()=> {
-  axios.get(`http://localhost:3000/products/${id}`).then((res)=> {
-    const productData = res.data
-    const lsCartItems = localStorage.getItem('cartItems');
-    const cartItems = lsCartItems ? JSON.parse(lsCartItems) : [];
-    productData.addedToCart = cartItems.includes(res.data.id);
-    setData(productData)
-  });
-  
+const [related, setRelated] = useState()
 
-}, [data])
+function get_related(){
+  axios.get(`http://localhost:2000/Products`)
+  .then((res) => {
+    console.log(res.data);
+    let related_data = res.data.filter((item) => item.id <= 3 );
+    setRelated(related_data);
+    })
+    .catch((e)=> console.log(e))
+}
+
+useEffect(() => {
+  getDetails();
+  get_related();
+}, []);
+
+
+  function getDetails() {
+    axios.get(`http://localhost:2000/Products/${id}`)
+    .then((res) => {
+      // Handle successful response here
+      const productData = res.data;
+      const lsCartItems = localStorage.getItem('cartItems');
+      const cartItems = lsCartItems ? JSON.parse(lsCartItems) : [];
+      productData.addedToCart = cartItems.includes(res.data.id);
+      setData(productData);
+    })
+    .catch((e) => console.log(e));
+  }
 
 
 
@@ -47,38 +68,12 @@ function slideImage(){
 
 window.addEventListener('resize', slideImage);
 
-const LikeUsOnFacebookButton = ({ url = "https://www.facebook.com/" }) => (
-  <a className="facebook_button" target="_blank" href={url}>
-    <FacebookSVG 
-      width="30px"
-      height="30px"
-      />
-    Like Us On Facebook!
-  </a>
-);
 
-const FacebookSVG = ({ style = {}, fill="#ffffff", className="", width="100%", height="100%", viewBox="0 0 408.788 408.788", d="" }) => (
-  <svg
-    width={width}
-    style={style}
-    height={height}
-    viewBox={viewBox}
-    className={className}
-    xmlns="http://www.w3.org/2000/svg"
-    xmlnsXlink="http://www.w3.org/1999/xlink"
-    >
-    <path fill={fill} 
-      d="M353.701,0H55.087C24.665,0,0.002,24.662,0.002,55.085v298.616c0,30.423,24.662,55.085,55.085,55.085
-       h147.275l0.251-146.078h-37.951c-4.932,0-8.935-3.988-8.954-8.92l-0.182-47.087c-0.019-4.959,3.996-8.989,8.955-8.989h37.882
-       v-45.498c0-52.8,32.247-81.55,79.348-81.55h38.65c4.945,0,8.955,4.009,8.955,8.955v39.704c0,4.944-4.007,8.952-8.95,8.955
-       l-23.719,0.011c-25.615,0-30.575,12.172-30.575,30.035v39.389h56.285c5.363,0,9.524,4.683,8.892,10.009l-5.581,47.087
-       c-0.534,4.506-4.355,7.901-8.892,7.901h-50.453l-0.251,146.078h87.631c30.422,0,55.084-24.662,55.084-55.084V55.085
-       C408.786,24.662,384.124,0,353.701,0z"
-      />
-  </svg>
-);
 
-  return <>
+
+
+  return  <>
+  <div >
    
    <div className = {styles.card_wrapper}>
   <div className = {styles.card}>
@@ -125,7 +120,7 @@ const FacebookSVG = ({ style = {}, fill="#ffffff", className="", width="100%", h
         <i className = "fas fa-star-half-alt"></i>
         </div>
         <span style={{color: 'rgb(204, 204, 204)'}}>{ data.rating?.count } reviews </span>
-        <span style={{color: '#00BFFF'}}>Submit a review </span>
+        <span style={{color: '#00BFFF'}}>Great Quality </span>
       </div>
 
 
@@ -223,7 +218,7 @@ const FacebookSVG = ({ style = {}, fill="#ffffff", className="", width="100%", h
 
 </div>
  
-  <ProductInfo/>
+  <ProductInfo data={data}/>
 
   <div>
     <div className={styles.relatedProducts}>
@@ -232,10 +227,71 @@ const FacebookSVG = ({ style = {}, fill="#ffffff", className="", width="100%", h
         Related Products
         </b>
        </h1>
+       <div className="container row">
+       {related.map((product) => (
+        
+          <div key={product.id} className="col-md-4  mt-4">
+            <div className={`card shadow-lg p-3 mb-5 bg-white rounded ${styles2.main_card}`}>
+              <div className={styles2.product_img_container}>
+                <span className={styles2.hot}>HOT</span>
+                <img
+                  className={`card-img-top ${styles2.product_img}`}
+                  src={product.imageURL}
+                  alt={product.name}
+                />
+                <div className="card-body text-start">
+                  
+                  <Link to={`/ProductDetails/${product.id}`} className={styles2.name}><h5 className={`card-title ${styles2.name}`} >{product.name}</h5></Link>
+                  <h3 className="card-title">{product.brand}</h3>
+                  <h3 className={`card-title ${styles2.realprice}`}>{product.price}$</h3>
+                  <h3 className={`card-title text-decoration-line-through ${styles2.price}`}>
+                    <i>{product.price * 1.2}$</i>
+                  </h3>
+                
+                  <a
+                    href="#"
+                    className={`btn btn-outline-success text-secondary me-5 ${styles2.card_icon}`}
+                    title="Add To Card"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      className="bi bi-bag"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
+                    </svg>
+                  </a>
+                  <a
+                    href="#"
+                    className={`btn btn-outline-danger text-danger ${styles2.love_icon}`}
+                    title="Add To Favorite"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      className="bi bi-heart"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
+                    </svg>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          
+        ))}
+        </div>
+
 
     </div>
   </div>
-
+  </div>
       
       
   </>;
